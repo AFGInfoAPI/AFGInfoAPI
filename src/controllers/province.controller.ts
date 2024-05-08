c8a178dd7f1ab8fa034f7364e50427a25c1c56cc
reza <ghrezam97@gmail.com>
2024-05-08 22:41:29 +0430

    Add province controller

---
+++ src/controllers/province.controller.ts
@@ -0,0 +1,31 @@
+import ProvinceService from '@/services/province.service';
+import { NextFunction, Request, Response } from 'express';
+
+class ProvinceController {
+  public provinceService = new ProvinceService();
+
+  public getProvinces = async (req: Request, res: Response, next: NextFunction) => {
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 10;
+
+    try {
+      const { provinces, total } = await this.provinceService.findAllProvinces(page, limit);
+      const totalPages = Math.ceil(total / limit);
+
+      res.status(200).json({
+        data: provinces,
+        meta: {
+          totalItems: total,
+          currentPage: page,
+          totalPages,
+          itemsPerPage: limit,
+        },
+        message: 'findAll',
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default ProvinceController;
