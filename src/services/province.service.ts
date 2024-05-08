8e76ff2fad975bc2064e1d27f2dbb6531f32aa7f
reza <ghrezam97@gmail.com>
2024-05-08 22:40:59 +0430

    Add province service

---
+++ src/services/province.service.ts
@@ -0,0 +1,35 @@
+import Province from '@/interfaces/province.interface';
+import { ProvinceModel } from '@/models/province.model';
+
+class ProvinceService {
+  public provinces = ProvinceModel;
+
+  public async findAllProvinces(page: number, limit: number): Promise<{ provinces: Province[]; total: number }> {
+    const skip = (page - 1) * limit;
+    const total = await this.provinces.countDocuments();
+    const provinces = await this.provinces.find().skip(skip).limit(limit);
+    return { provinces, total };
+  }
+
+  public async findProvinceById(provinceId: string) {
+    const province = await this.provinces.findOne({ _id: provinceId });
+    return province;
+  }
+
+  public async createProvince(provinceData) {
+    const createProvince = await this.provinces.create(provinceData);
+    return createProvince;
+  }
+
+  public async updateProvince(provinceId: string, provinceData) {
+    const updateProvince = await this.provinces.findByIdAndUpdate(provinceId, provinceData);
+    return updateProvince;
+  }
+
+  public async deleteProvince(provinceId: string) {
+    const deleteProvince = await this.provinces.findByIdAndDelete(provinceId);
+    return deleteProvince;
+  }
+}
+
+export default ProvinceService;
