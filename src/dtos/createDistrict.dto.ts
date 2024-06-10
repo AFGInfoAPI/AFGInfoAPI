ee6cce89978ef5caa3b77ca9453e8675f4fb40d6
Ali Baba <ali7090370@gmail.com>
2024-06-10 19:07:38 +0430

    feat: Add CreateDistrictDto for creating a new district

---
+++ src/dtos/createDistrict.dto.ts
@@ -0,0 +1,33 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateDistrictDto {
+  @IsString()
+  readonly name: string;
+
+  @IsNumber()
+  readonly area: number;
+
+  @IsNumber()
+  readonly population: number;
+
+  @IsNumber()
+  readonly gdp: number;
+
+  @IsNumber()
+  readonly lat: number;
+
+  @IsNumber()
+  readonly lng: number;
+
+  @IsString()
+  readonly googleMapUrl: string;
+
+  @IsString()
+  readonly capital: string;
+
+  @IsString()
+  readonly description: string;
+
+  @IsString({ each: true })
+  readonly images: string[];
+}
