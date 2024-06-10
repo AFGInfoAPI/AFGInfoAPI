de8a490b74de5fd1a6e7666446a0410fe459715c
Ali Baba <ali7090370@gmail.com>
2024-06-10 19:10:55 +0430

    refactor: Update district route to include authentication middleware and validation for creating districts

--- src/routes/district.route.ts
+++ src/routes/district.route.ts
@@ -1,12 +1,16 @@
 import DistrictController from '@/controllers/district.controller';
 import { Router } from 'express';
 import multer from 'multer';
+import { createDistrictValidation } from '@/middlewares/create.district.middleware';
+import { validateFile } from '@/middlewares/filevalidator.middleware';
+import nearByValidation from '@/middlewares/nearby.validation.middleware';
+import authMiddleware from '@/middlewares/auth.middleware';
+import { HttpException } from '@/exceptions/HttpException';
 
 class DistrictRoute {
   public path = '/districts';
   public router = Router();
   public districtController = new DistrictController();
-
   private storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'uploads/');
@@ -23,12 +27,13 @@ class DistrictRoute {
       if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
         cb(null, true);
       } else {
-        cb(new Error(`${file.fieldname}: File type is not supported`));
+        cb(new HttpException(400, 'File type is not supported', { [file.fieldname]: 'File type is not supported' }));
       }
     },
   });
 
   constructor() {
+    this.router.use(authMiddleware);
     this.initializeRoutes();
   }
 
@@ -41,15 +46,16 @@ class DistrictRoute {
       .fill(0)
       .map((_, i) => ({ name: `images[${i}]` }));
 
-    uploadRouter.post('/', this.upload.fields(fields), this.districtController.createDistrict);
-    uploadRouter.patch('/:id', this.upload.fields(fields), this.districtController.updateDistrict);
+    uploadRouter.post('/', this.upload.fields(fields), createDistrictValidation, this.districtController.createDistrict);
+    uploadRouter.patch('/:id', this.upload.fields(fields), createDistrictValidation, this.districtController.updateDistrict);
     uploadRouter.delete('/:id', this.districtController.deleteDistrict);
     uploadRouter.patch('/:id/images', this.upload.fields(fields), this.districtController.updateDistrictImages);
     uploadRouter.delete('/:id/images/:image_name', this.districtController.deleteDistrictImage);
 
     // Use the upload router without multer middleware
     this.router.use(`${this.path}`, uploadRouter);
-
+    this.router.post(`${this.path}/approve/:id`, this.districtController.approveDistrict);
+    this.router.get(`${this.path}/nearbyDistricts`, nearByValidation, this.districtController.getNearbyDistricts);
     this.router.get(`${this.path}`, this.districtController.getDistricts);
     this.router.get(`${this.path}/:id`, this.districtController.getDistrictById);
   }
