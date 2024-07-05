7fe5c0cbcd1db11e06ec9f65d9fd97eff42d8b56
Ali Baba <ali7090370@gmail.com>
2024-07-05 09:41:35 +0430

    feat: Add TouristicPlace route to server configuration

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,10 @@ import UsersRoute from '@routes/users.route';
 import validateEnv from '@utils/validateEnv';
 import ProvinceRoute from './routes/province.route';
 import DistrictRoute from './routes/district.route';
+import TouristicPlace from './routes/touristicplace.route';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new ProvinceRoute(), new DistrictRoute()]);
+const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new ProvinceRoute(), new DistrictRoute(), new TouristicPlace()]);
 
 app.listen();
