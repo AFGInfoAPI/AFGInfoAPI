b46d92c1b4bd68e7afbacde5138f177651d10d6c
Ali Baba <ali7090370@gmail.com>
2024-07-09 22:58:51 +0430

    Add ParkPndService and ParkService classes This commit adds two new service classes, `ParkPndService` and `ParkService`, to handle park-related operations.

---
+++ src/services/park.pend.service.ts
@@ -0,0 +1,24 @@
+import { ParkPndModel } from '@/models/park.pend.model';
+import BaseService from './base.service';
+import { ParkPnd } from '@/interfaces/park.interface';
+
+class ParkPndService extends BaseService<ParkPnd> {
+  public parks = ParkPndModel;
+  constructor() {
+    super(ParkPndModel);
+  }
+
+  // Add the findOne method
+  public async findOne(queryObject, projectObj): Promise<ParkPnd | null> {
+    try {
+      const result = await this.parks.findOne(queryObject, projectObj).exec();
+      return result;
+    } catch (error) {
+      // Log the error or handle it as needed
+      console.error('Error finding document:', error);
+      throw error;
+    }
+  }
+}
+
+export default ParkPndService;
